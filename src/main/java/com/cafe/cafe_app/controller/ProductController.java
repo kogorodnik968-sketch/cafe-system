a1ef77df016596550abc9b8c84c055b6fb2e6462
katee <kogorodnik968@gmail.com>
2026-02-23 23:10:30 +0300

    Initial commit

--- src/main/java/com/cafe/cafe_app/controller/ProductController.java
+++ src/main/java/com/cafe/cafe_app/controller/ProductController.java
@@ -3,7 +3,11 @@ package com.cafe.cafe_app.controller;
 import com.cafe.cafe_app.dto.ProductDto;
 import com.cafe.cafe_app.service.ProductService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
