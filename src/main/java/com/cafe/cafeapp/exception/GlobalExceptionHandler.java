076756dae335d1a5d13b2df8b4fb3f2e5def7ece
katee <kogorodnik968@gmail.com>
2026-04-07 16:31:42 +0300

    lab4

--- src/main/java/com/cafe/cafeapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cafe/cafeapp/exception/GlobalExceptionHandler.java
@@ -1,21 +1,24 @@
 package com.cafe.cafeapp.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.time.LocalDateTime;
 
-@ControllerAdvice
+@RestControllerAdvice
+@Slf4j
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<ErrorResponse> handleValidation (
             MethodArgumentNotValidException exc, HttpServletRequest request) {
 
+        log.error("Ошибка валидации: ", exc);
         String message = exc.getBindingResult().getFieldErrors().stream().
                 map(error -> error.getField() + ": " + error.getDefaultMessage()).
                 findFirst().orElse("Validation error");
@@ -30,7 +33,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<ErrorResponse> handleInvalidFormat(
             HttpMessageNotReadableException ex,
             HttpServletRequest request) {
-
+        log.error("Неверный формат данных: ", ex);
         ErrorResponse error = new ErrorResponse(
                 LocalDateTime.now(),
                 400,
@@ -45,6 +48,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(NotFoundException.class)
     public ResponseEntity<ErrorResponse> handleNotFound (
             NotFoundException exc, HttpServletRequest request) {
+        log.error("Не найдено: ", exc);
         ErrorResponse error = new ErrorResponse( LocalDateTime.now(), 404, "Not Found",
                 exc.getMessage(), request.getRequestURI());
         return ResponseEntity.status(404).body(error);
@@ -53,6 +57,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(AlreadyExistsException.class)
     public ResponseEntity<ErrorResponse> handleAlreadyExists (
             AlreadyExistsException exc, HttpServletRequest request) {
+        log.error("Таакой объект уже создан: ", exc);
         ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 409, "Conflict",
                 exc.getMessage(), request.getRequestURI());
 
@@ -62,6 +67,7 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(DeleteNotAllowedException.class)
     public ResponseEntity<ErrorResponse> handleDeleteNotAllowed (
             DeleteNotAllowedException exc, HttpServletRequest request) {
+        log.error("Невозможно удалить: ", exc);
         return ResponseEntity.status(409).body(
                 new ErrorResponse(LocalDateTime.now(), 409, "Conflict",
                         exc.getMessage(), request.getRequestURI()));
@@ -69,6 +75,8 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ErrorResponse> handleGeneric ( Exception exc, HttpServletRequest request) {
+
+        log.error("Произошла ошибка: ", exc);
         ErrorResponse error = new ErrorResponse(LocalDateTime.now(), 500, "Internal server Error",
                 "Что-то пошло не так", request.getRequestURI());
 
